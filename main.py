aa79b53cb609ce77b687805ff047f8ab736d3066
Amanda <amanda.vilalva@escola.pr.gov.br>
2024-12-04 16:43:20 -0300

    Funcionalidade de gerar senhas implementada

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import base64
+import random
+import string
 
 def armazenar_senha(servico, senha):
     if not os.path.exists("senhas.txt"):
@@ -8,3 +10,9 @@ def armazenar_senha(servico, senha):
     senha_codificada = base64.b64encode(senha.encode("utf-8")).decode("utf-8")
     with open("senhas.txt", "a") as f:
         f.write(f"{servico}:{senha_codificada}\n")
+
+def gerar_senha(segura=True, tamanho=12):
+    caracteres = string.ascii_letters + string.digits
+    if segura:
+        caracteres += string.punctuation
+    return ''.join(random.choice(caracteres) for _ in range(tamanho))
